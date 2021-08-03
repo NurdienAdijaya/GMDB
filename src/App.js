0c5f193cab6ab823282b4bb79319980f8dbb6a9a
Nurdien Adijaya <nurdienadijaya@gmail.com>
2021-08-03 21:28:00 +0700

    setting router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PostReview from "./components/PostReview";
-import AllReview from "./components/AllReview";
 import { Provider } from "react-redux";
 import store from "./store";
-import Main from "./pages/Main";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { BrowserRouter } from "react-router-dom";
+import Routers from "./Routes/router";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Header />
-        <Main />
-        <PostReview />
-        <AllReview />
-        <Footer />
+        <Routers />
       </BrowserRouter>
     </Provider>
-  )
+  );
 }
 
 export default App;
