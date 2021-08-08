3ebefedb30d153146b9795acaeb808611880e06a
Bima Gofarali Roby Kusuma Harta <bimagofaralirobi@gmail.com>
2021-08-08 13:43:23 +0700

    get Actors

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -5,13 +5,11 @@ import reducerSearch from "./searchMovie";
 import detail from "./detail";
 import reducerMovieNurd from "./moviePlaying";
 import reducerUser from "./user";
-import reducerActors from "./Actor"
 const rootReducers = combineReducers({
   reducerReview,
   movie,
   reducerSearch,
   detail,
-  reducerActors,
   reducerMovieNurd,
   reducerUser,
 });
